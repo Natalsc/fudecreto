a8b35c564629a303d414ef9e1fb6e011d12b675a
Natalsc <natal.sc@outlook.com>
2016-10-17 12:15:20 -0200

    first commit

---
+++ Futeba/Futeba/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Futeba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.gridtime1 = new System.Windows.Forms.DataGridView();
+            this.gridTime2 = new System.Windows.Forms.DataGridView();
+            this.gridTime3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridtime1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTime2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTime3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Location = new System.Drawing.Point(12, 12);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(75, 23);
+            this.btnRandomize.TabIndex = 0;
+            this.btnRandomize.Text = "Tirar Time";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnRandomize_Click);
+            // 
+            // gridtime1
+            // 
+            this.gridtime1.AllowUserToAddRows = false;
+            this.gridtime1.AllowUserToDeleteRows = false;
+            this.gridtime1.AllowUserToResizeColumns = false;
+            this.gridtime1.AllowUserToResizeRows = false;
+            this.gridtime1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridtime1.Location = new System.Drawing.Point(12, 59);
+            this.gridtime1.Name = "gridtime1";
+            this.gridtime1.RowHeadersVisible = false;
+            this.gridtime1.Size = new System.Drawing.Size(215, 180);
+            this.gridtime1.TabIndex = 1;
+            // 
+            // gridTime2
+            // 
+            this.gridTime2.AllowUserToAddRows = false;
+            this.gridTime2.AllowUserToDeleteRows = false;
+            this.gridTime2.AllowUserToResizeColumns = false;
+            this.gridTime2.AllowUserToResizeRows = false;
+            this.gridTime2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTime2.Location = new System.Drawing.Point(233, 59);
+            this.gridTime2.Name = "gridTime2";
+            this.gridTime2.RowHeadersVisible = false;
+            this.gridTime2.Size = new System.Drawing.Size(215, 180);
+            this.gridTime2.TabIndex = 1;
+            // 
+            // gridTime3
+            // 
+            this.gridTime3.AllowUserToAddRows = false;
+            this.gridTime3.AllowUserToDeleteRows = false;
+            this.gridTime3.AllowUserToResizeColumns = false;
+            this.gridTime3.AllowUserToResizeRows = false;
+            this.gridTime3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTime3.Location = new System.Drawing.Point(454, 59);
+            this.gridTime3.Name = "gridTime3";
+            this.gridTime3.RowHeadersVisible = false;
+            this.gridTime3.Size = new System.Drawing.Size(215, 180);
+            this.gridTime3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 251);
+            this.Controls.Add(this.gridTime3);
+            this.Controls.Add(this.gridTime2);
+            this.Controls.Add(this.gridtime1);
+            this.Controls.Add(this.btnRandomize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridtime1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTime2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTime3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.DataGridView gridtime1;
+        private System.Windows.Forms.DataGridView gridTime2;
+        private System.Windows.Forms.DataGridView gridTime3;
+    }
+}
+
---
+++ Futeba/Futeba/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Futeba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random _random = new Random();
+        DataTable _players = new DataTable();
+
+        private void Randomize()
+        {
+            var times = _players.Rows.Count/6;
+            var timeLevel = 0;
+            foreach (DataRow players in _players.Rows)
+            {
+                timeLevel += int.Parse(players["level"].ToString());
+            }
+
+            timeLevel = timeLevel / times;
+
+            var sort = "level desc";
+            for (var i = 1; i < 7; i++)
+            {
+                var expression = $"position = {i}";
+                var foundRows = _players.Select(expression);
+
+                var playersName = "";
+                var iCount = 1;
+                foreach (var row in foundRows)
+                {
+                    if (row["time"].ToString() != string.Empty)continue;
+                    playersName += $"{row["name"]},";
+                }
+
+                var array = playersName.Split(',');
+
+                Shuffle(array);
+                foreach (var value in array)
+                {
+                    if (string.IsNullOrEmpty(value)) continue;
+                    foreach (DataRow players in _players.Rows)
+                    {
+                        if (value == players["name"].ToString())
+                            players["time"] = iCount;
+                    }
+                    if (iCount == times)
+                        break;
+
+                    iCount++;
+                }
+            }
+
+            var time1 = new DataTable();
+            time1.Columns.Add("name", typeof(string));
+            time1.Columns.Add("level", typeof(int));
+            var time2 = new DataTable();
+            time2.Columns.Add("name", typeof(string));
+            time2.Columns.Add("level", typeof(int));
+            var time3 = new DataTable();
+            time3.Columns.Add("name", typeof(string));
+            time3.Columns.Add("level", typeof(int));
+
+            for (var i = 1; i <= 3; i++)
+            {
+                var expression = $"time = {i}";
+                var foundRows = _players.Select(expression);
+                
+                foreach (var rows in foundRows)
+                {
+                    if (rows["time"].ToString() == "1")
+                    {
+                        var row = time1.NewRow();
+                        row["name"] = rows["name"];
+                        row["level"] = rows["level"];
+                        time1.Rows.Add(row);
+                    }
+
+                    if (rows["time"].ToString() == "2")
+                    {
+                        var row = time2.NewRow();
+                        row["name"] = rows["name"];
+                        row["level"] = rows["level"];
+                        time2.Rows.Add(row);
+                    }
+
+                    if (rows["time"].ToString() == "3")
+                    {
+                        var row = time3.NewRow();
+                        row["name"] = rows["name"];
+                        row["level"] = rows["level"];
+                        time3.Rows.Add(row);
+                    }
+                }
+            }
+
+            gridtime1.DataSource = time1;
+            gridTime2.DataSource = time2;
+            gridTime3.DataSource = time3;
+        }
+
+        private void Shuffle<T>(T[] array)
+        {
+            var random = _random;
+            for (var i = array.Length; i > 1; i--)
+            {
+                var j = random.Next(i);
+                var tmp = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = tmp;
+            }
+        }
+
+        private void btnRandomize_Click(object sender, EventArgs e)
+        {
+            Randomize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _players.Columns.Add("name", typeof (string));
+            _players.Columns.Add("position", typeof(string));
+            _players.Columns.Add("level", typeof(int));
+            _players.Columns.Add("time", typeof(string));
+
+            var row = _players.NewRow();
+            row["name"] = "GOLEIRO 1";
+            row["position"] = "1";
+            row["level"] = 5;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "GOLEIRO 2";
+            row["position"] = "1";
+            row["level"] = 7;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "GOLEIRO 3";
+            row["position"] = "1";
+            row["level"] = 3;
+            _players.Rows.Add(row);
+
+
+
+            row = _players.NewRow();
+            row["name"] = "NATAL";
+            row["position"] = "2";
+            row["level"] = 3;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "SOARES";
+            row["position"] = "2";
+            row["level"] = 6;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "BOCA";
+            row["position"] = "2";
+            row["level"] = 1;
+            _players.Rows.Add(row);
+
+
+
+            row = _players.NewRow();
+            row["name"] = "PET";
+            row["position"] = "3";
+            row["level"] = 10;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "JUNIOR";
+            row["position"] = "3";
+            row["level"] = 4;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "NEGO";
+            row["position"] = "3";
+            row["level"] = 7;
+            _players.Rows.Add(row);
+
+
+
+            row = _players.NewRow();
+            row["name"] = "NEY";
+            row["position"] = "4";
+            row["level"] = 5;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "BOI";
+            row["position"] = "4";
+            row["level"] = 7;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "DRI";
+            row["position"] = "4";
+            row["level"] = 3;
+            _players.Rows.Add(row);
+
+
+
+            row = _players.NewRow();
+            row["name"] = "CHRISTOFOLETTI";
+            row["position"] = "5";
+            row["level"] = 3;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "SAFADAO";
+            row["position"] = "5";
+            row["level"] = 6;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "DU";
+            row["position"] = "5";
+            row["level"] = 1;
+            _players.Rows.Add(row);
+
+
+
+            row = _players.NewRow();
+            row["name"] = "PIMENTA";
+            row["position"] = "6";
+            row["level"] = 10;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "BOY";
+            row["position"] = "6";
+            row["level"] = 4;
+            _players.Rows.Add(row);
+
+            row = _players.NewRow();
+            row["name"] = "CHRIS";
+            row["position"] = "6";
+            row["level"] = 7;
+            _players.Rows.Add(row);
+
+
+        }
+    }
+}
