8eccf49641a56b6dc5648ffca71a6c8bc2d7948a
Natalsc <natal.sc@outlook.com>
2016-10-21 14:53:13 -0200

    Futeba

---
+++ DescubraApp/DescubraApp/Activities/MainActivity.cs
@@ -0,0 +1,102 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Views;
+using DescubraApp.Fragments;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace DescubraApp.Activities
+{
+    [Activity(Label = "Menu Futeba", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, Icon = "@drawable/Icon")]
+    public class MainActivity : BaseActivity
+    {
+        DrawerLayout _drawerLayout;
+        NavigationView _navigationView;
+
+        protected override int LayoutResource => Resource.Layout.main;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            _drawerLayout = this.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            //Set hamburger items menu
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+
+            //setup navigation view
+            _navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+
+            //handle navigation
+            _navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                e.MenuItem.SetChecked(true);
+
+                switch (e.MenuItem.ItemId)
+                {
+                    case Resource.Id.nav_home_1:
+                        ListItemClicked(0);
+                        break;
+                    case Resource.Id.nav_home_2:
+                        ListItemClicked(1);
+                        break;
+                    case Resource.Id.nav_home_3:
+                        ListItemClicked(2);
+                        break;
+                }
+
+                _drawerLayout.CloseDrawers();
+            };
+
+            //if first time you will want to go ahead and click first item.
+            if (savedInstanceState == null)
+            {
+                ListItemClicked(0);
+            }
+        }
+
+        int _oldPosition = -1;
+        private void ListItemClicked(int position)
+        {
+            //this way we don't load twice, but you might want to modify this a bit.
+            if (position == _oldPosition)
+                return;
+
+            _oldPosition = position;
+
+            Fragment fragment = null;
+            switch (position)
+            {
+                case 0:
+                    fragment = Fragment1.NewInstance();
+                    break;
+                case 1:
+                    fragment = Fragment2.NewInstance();
+                    break;
+                case 2:
+                    fragment = Fragment3.NewInstance();
+                    break;
+            }
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content_frame, fragment)
+                .Commit();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    _drawerLayout.OpenDrawer(GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
